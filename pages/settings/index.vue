ec16df35af58ed0025cdf25a86f897285c550a05
szejing <szejing.go@gmail.com>
2024-09-15 14:34:21 +0800

    added dashboard components

--- pages/settings/index.vue
+++ pages/settings/index.vue
@@ -1,9 +1,18 @@
 <template>
-	<div class="">
+	<div>
+		<UBreadcrumb :links="links" />
 		<h1 class="font-nunito text-2xl">Settings</h1>
 	</div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const links = [
+	{
+		label: 'Settings',
+		icon: 'i-material-symbols-light-settings-outline-rounded',
+		to: '/',
+	},
+];
+</script>
 
 <style scoped lang="css"></style>
--- pages/store/index.vue
+++ pages/store/index.vue
@@ -1,9 +1,18 @@
 <template>
-	<div class="">
+	<div>
+		<UBreadcrumb :links="links" />
 		<h1 class="font-nunito text-2xl">Store</h1>
 	</div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const links = [
+	{
+		label: 'Overview',
+		icon: 'i-material-symbols-light-dashboard-outline-rounded',
+		to: '/',
+	},
+];
+</script>
 
 <style scoped lang="css"></style>
