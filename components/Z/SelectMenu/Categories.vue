40fecec2738f41ca3fe9c13b1210ef866af0932d
szejing <szejing.go@gmail.com>
2024-09-21 12:57:06 +0800

    - added product categories - added product tags - added options and settings store

---
+++ components/Z/SelectMenu/Categories.vue
@@ -0,0 +1,37 @@
+<template>
+	<UFormGroup name="categories" class="mt-2">
+		<USelectMenu
+			v-model="categories"
+			v-model:query="query"
+			:options="options_categories"
+			searchable
+			size="md"
+			value-attribute="name"
+			option-attribute="name"
+			multiple
+		>
+			<template #label>
+				<span v-if="categories.length" class="truncate">{{ categories.join(', ') }}</span>
+				<span v-else>Select Categories</span>
+			</template>
+		</USelectMenu>
+	</UFormGroup>
+</template>
+
+<script lang="ts" setup>
+const options_categories = [
+	{
+		id: 0,
+		name: 'Spare Parts',
+	},
+	{
+		id: 1,
+		name: 'Services',
+	},
+];
+
+const query = ref('');
+const categories = ref([]);
+</script>
+
+<style></style>
