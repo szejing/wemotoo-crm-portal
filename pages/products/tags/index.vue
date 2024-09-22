dba995e89bdfbf269195d5c76867218854735eab
szejing <szejing.go@gmail.com>
2024-09-22 09:39:56 +0800

    added product breadcrumbs

--- pages/products/tags/index.vue
+++ pages/products/tags/index.vue
@@ -1,13 +1,26 @@
 <template>
-  <div>
-    
-  </div>
+	<div>
+		<UBreadcrumb :links="links" />
+		<div class="container">
+			<ZSectionFilterProducts />
+			<UCard class="mt-4"> </UCard>
+		</div>
+	</div>
 </template>
 
 <script lang="ts" setup>
-
+const links = [
+	{
+		label: 'Products',
+		icon: 'i-material-symbols-light-box-add-outline-rounded',
+		to: '/products',
+	},
+	{
+		label: 'All Tags',
+		icon: 'i-material-symbols-light-lists-rounded',
+		to: '/tags',
+	},
+];
 </script>
 
-<style>
-
-</style>
+<style scoped lang="css"></style>
