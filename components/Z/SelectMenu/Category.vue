21bdffaaa0d15473b824d4ecb18c537b69b14567
szejing <szejing.go@gmail.com>
2024-12-15 14:04:58 +0800

    ignore category code

--- components/Z/SelectMenu/Category.vue
+++ components/Z/SelectMenu/Category.vue
@@ -3,13 +3,19 @@
 		<USelectMenu
 			v-model="category"
 			v-model:query="query"
-			:options="productCategories"
+			:options="availableCategories"
 			searchable
 			size="md"
 			value-attribute="code"
-			option-attribute="name"
 			placeholder="Select Category"
-		/>
+		>
+			<template #option="{ option: catg }">
+				<div class="flex items-center gap-2">
+					<p class="text-neutral-300 font-light">[{{ catg.code }}]</p>
+					<p class="text-neutral-700 font-semibold">{{ catg.name }}</p>
+				</div>
+			</template>
+		</USelectMenu>
 	</UFormGroup>
 </template>
 
@@ -20,7 +26,11 @@ const query = ref('');
 const categoryStore = useProductCategoriesStore();
 const { productCategories } = storeToRefs(categoryStore);
 
-const props = defineProps<{ category: ProductCategory | undefined }>();
+const props = defineProps<{ category: ProductCategory | undefined; ignoreCodes?: string[] }>();
+
+const availableCategories = computed(() => {
+	return productCategories.value.filter((catg) => !props.ignoreCodes?.includes(catg.code));
+});
 
 const emit = defineEmits(['update:category']);
 
