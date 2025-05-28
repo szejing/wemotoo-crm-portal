6ab66da9ce14538bc28076d5dcfc274b98ac1924
szejing <szejing.go@gmail.com>
2025-05-28 21:04:48 +0800

    Enhancement: Optimize initialization process in App store
    - Refactored the init action to use Promise.all for concurrent fetching of data from multiple stores, improving performance and reducing loading time.

--- stores/App.ts
+++ stores/App.ts
@@ -5,43 +5,34 @@ export const useAppStore = defineStore('appStore', {
 	actions: {
 		async init() {
 			const productOptionsStore = useProductOptionStore();
-			await productOptionsStore.getOptions();
-
 			const productTagsStore = useProductTagStore();
-			await productTagsStore.getTags();
-
 			const categoryStore = useProductCategoryStore();
-			await categoryStore.getCategories();
-
 			const productStore = useProductStore();
-			await productStore.getProducts();
-
 			const merchantInfo = useMerchantInfoStore();
-			await merchantInfo.getCurrencies();
-
 			const settingsStore = useSettingStore();
-			await settingsStore.getSettings();
-
 			const productTypeStore = useProductTypeStore();
-			await productTypeStore.getProductTypes();
-
 			const orderStore = useOrderStore();
-			await orderStore.getOrders();
-
 			const paymentTypeStore = usePaymentTypeStore();
-			await paymentTypeStore.getPaymentTypeGroups();
-
 			const paymentMethodStore = usePaymentMethodStore();
-			await paymentMethodStore.getPaymentMethods();
-
 			const summOrderStore = useSummOrderStore();
-			await summOrderStore.getDashboardSummary();
-
 			const summSalesStore = useSummSaleStore();
-			await summSalesStore.getDashboardSummary();
-
 			const customerStore = useCustomerStore();
-			await customerStore.getCustomers();
+
+			Promise.all([
+				productOptionsStore.getOptions(),
+				productTagsStore.getTags(),
+				categoryStore.getCategories(),
+				productStore.getProducts(),
+				merchantInfo.getCurrencies(),
+				settingsStore.getSettings(),
+				productTypeStore.getProductTypes(),
+				orderStore.getOrders(),
+				paymentTypeStore.getPaymentTypeGroups(),
+				paymentMethodStore.getPaymentMethods(),
+				summOrderStore.getDashboardSummary(),
+				summSalesStore.getDashboardSummary(),
+				customerStore.getCustomers(),
+			]);
 		},
 	},
 });
